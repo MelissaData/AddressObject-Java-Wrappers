1683f06681ea09c69ec6ab08da06fb4641af40b7
Melissa <>
2024-12-18 08:30:55 -0800

    Tested successfully using Melissa object for 2024.12

--- BuildAddrJavaWrapper.ps1
+++ BuildAddrJavaWrapper.ps1
@@ -15,7 +15,7 @@ class FileConfig {
 
 ######################### Config ###########################
 
-$RELEASE_VERSION = '2024.11'
+$RELEASE_VERSION = '2024.12'
 
 $CurrentPath = $PSScriptRoot
 $CmdPath = "C:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvarsall.bat" # Configure the path to vcvarsall.bat if needed
--- BuildAddrJavaWrapper.sh
+++ BuildAddrJavaWrapper.sh
@@ -14,7 +14,7 @@ while [ $# -gt 0 ] ; do
 done
 
 ########################## Config ###########################
-RELEASE_VERSION='2024.11'
+RELEASE_VERSION='2024.12'
 
 ProjectPath=$(pwd)
 
